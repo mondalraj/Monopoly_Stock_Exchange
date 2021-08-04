b13edb032c908c378e73be1e3a9839bf8e78434c
Rajib Mondal <mondal021002rajib@gmail.com>
2021-08-04 22:06:20 +0530

    Getter/Setter of player name completed

--- src/functions/Function.java
+++ src/functions/Function.java
@@ -3,15 +3,19 @@ package functions;
 import java.util.*;
 
 public class Function {
-    public int welcome(){
+    public int welcome() {
         System.out.println("Welcome");
         return 0;
     }
-    public String chance(){
-          String[] chanceName={"Hostile takeover attempt - pay lawyer 1000","Your least expensive ticket -pay 5 times share price to bank and inc","Your most expensive ticket - pay 2 times share price to bak and inc","Go to trading allowed counter , pass go (nearest trading counter)."};
+
+    public String chance() {
+        String[] chanceName = { "Hostile takeover attempt - pay lawyer 1000",
+                "Your least expensive ticket -pay 5 times share price to bank and inc",
+                "Your most expensive ticket - pay 2 times share price to bak and inc",
+                "Go to trading allowed counter , pass go (nearest trading counter)." };
         Random random = new Random();
-        int randomNumber=random.nextInt(chanceName.length);
-        String s=chanceName[randomNumber];
+        int randomNumber = random.nextInt(chanceName.length);
+        String s = chanceName[randomNumber];
         System.out.println(s);
         return s;
     }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,70 +1,68 @@
 package player;
 
 public class Player {
-    public void setPlayerName(String name , int n){
+    Player1 p1 = new Player1();
+    Player2 p2 = new Player2();
+    Player3 p3 = new Player3();
 
-        if(n == 1 ){
-            Player1 p1 = new Player1();
+    public void setPlayerName(String name, int n) {
+
+        if (n == 1) {
             p1.setName(name);
-        } else if(n == 2){
-            Player2 p2 = new Player2();
+        } else if (n == 2) {
             p2.setName(name);
-        } else if(n == 3){
-            Player3 p3 = new Player3();
+        } else if (n == 3) {
             p3.setName(name);
         }
-    
+
     }
 
-    public void getPlayerName( int n){
-        if(n == 1){
-            Player1 p1 = new Player1();
-            System.out.println(p1.getName());
-            
-        } else if(n == 2 ){
-            Player2 p2 = new Player2();
-            System.out.println(p2.getName());
+    public void getPlayerName(int n) {
+        if (n == 1) {
+            System.out.println("Name of Player 1 is : " + p1.getName());
 
-        } else{
-            Player3 p3 = new Player3();
-            System.out.println(p3.getName());
-            
+        } else if (n == 2) {
+            System.out.println("Name of Player 2 is : " + p2.getName());
+
+        } else {
+            System.out.println("Name of Player 3 is : " + p3.getName());
+
         }
     }
 }
 
-class Player1{
+class Player1 {
     public String name;
 
-    public void setName(String name1){
+    public void setName(String name1) {
         this.name = name1;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 }
 
-class Player2{
+class Player2 {
     public String name;
 
-    public void setName(String name2){
+    public void setName(String name2) {
         this.name = name2;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 }
 
-class Player3{
+class Player3 {
     public String name;
 
-    public void setName(String name3){
+    public void setName(String name3) {
         this.name = name3;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 }
