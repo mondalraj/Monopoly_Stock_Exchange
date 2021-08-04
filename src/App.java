788b17e3e3c49b2c04ac30874c3c7a87ca74499d
Rajib Mondal <mondal021002rajib@gmail.com>
2021-08-04 21:00:17 +0530

    Getting and setting player name

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 import functions.Function;
+import player.Player;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,5 +9,22 @@ public class App {
         Function fun = new Function();
         fun.welcome();
         fun.chance();
+        Player player = new Player();
+
+        // Setting Player Names
+        Scanner sc = new Scanner(System.in);
+        String name1 = sc.nextLine();
+        player.setPlayerName(name1, 1);
+
+        String name2 = sc.nextLine();
+        player.setPlayerName(name2, 2);
+
+        String name3 = sc.nextLine();
+        player.setPlayerName(name3, 3);
+
+        //Getting Player names
+        player.getPlayerName(1);
+        player.getPlayerName(2);
+        player.getPlayerName(3);
     }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,5 +1,70 @@
 package player;
 
 public class Player {
+    public void setPlayerName(String name , int n){
+
+        if(n == 1 ){
+            Player1 p1 = new Player1();
+            p1.setName(name);
+        } else if(n == 2){
+            Player2 p2 = new Player2();
+            p2.setName(name);
+        } else if(n == 3){
+            Player3 p3 = new Player3();
+            p3.setName(name);
+        }
     
+    }
+
+    public void getPlayerName( int n){
+        if(n == 1){
+            Player1 p1 = new Player1();
+            System.out.println(p1.getName());
+            
+        } else if(n == 2 ){
+            Player2 p2 = new Player2();
+            System.out.println(p2.getName());
+
+        } else{
+            Player3 p3 = new Player3();
+            System.out.println(p3.getName());
+            
+        }
+    }
+}
+
+class Player1{
+    public String name;
+
+    public void setName(String name1){
+        this.name = name1;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
+
+class Player2{
+    public String name;
+
+    public void setName(String name2){
+        this.name = name2;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
+
+class Player3{
+    public String name;
+
+    public void setName(String name3){
+        this.name = name3;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
