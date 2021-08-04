3075ef9bd80328720349a794247b2d90ef7444a0
Rajib Mondal <mondal021002rajib@gmail.com>
2021-08-04 14:00:38 +0530

    chance function declared

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import functions.Function;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Function fun = new Function();
+        fun.welcome();
+        fun.chance();
     }
 }
