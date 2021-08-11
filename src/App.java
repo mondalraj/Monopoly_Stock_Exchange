09339894df05036604272afed45624e20d244d73
Rajib Mondal <mondal021002rajib@gmail.com>
2021-08-11 22:07:33 +0530

    Day 1 of main function

--- src/App.java
+++ src/App.java
@@ -6,78 +6,50 @@ import player.Player;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Function fun = new Function();
-        Player player = new Player();
-        Company company = new Company();
-
-        fun.welcome();
-        fun.chance();
-
-        // Setting Player Names
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Player 1 Name : ");
-        String name1 = sc.nextLine();
+        Function func = new Function();
+        Player player = new Player();
+        
+        /*
+        func.welcome();
+
+        func.instructions();
+
+        System.out.print("Enter the First Player's name:- ");
+        String name1  = sc.nextLine();
         player.setPlayerName(name1, 1);
 
-        System.out.print("Enter Player 2 Name : ");
-        String name2 = sc.nextLine();
+        System.out.print("Enter the Second Player's name:- ");
+        String name2  = sc.nextLine();
         player.setPlayerName(name2, 2);
 
-        System.out.print("Enter Player 3 Name : ");
-        String name3 = sc.nextLine();
+        System.out.print("Enter the Third Player's name:- ");
+        String name3  = sc.nextLine();
         player.setPlayerName(name3, 3);
 
-        // Getting Player names
-        player.getPlayerName(1);
-        player.getPlayerName(2);
-        player.getPlayerName(3);
+        System.out.println("\n Press Enter to start the game... ");
 
-        // quit function
-        fun.quit();
-
-        // winner function
-        fun.winner();
-
-        // Setting Player's currentBalance
-        player.setPlayerCurrentBal(25000, 1);
-        player.setPlayerCurrentBal(2000, 2);
-        player.setPlayerCurrentBal(2000, 3);
-
-        // Getting Player's currentBalance
-        System.out.println(player.getPlayerCurrentBal(1));
+        func.board();
         
-        player.getPlayerCurrentBal(2);
-        player.getPlayerCurrentBal(3);
 
-        // Company Notation -> a2 - Amazon, t5 - tesla, m6 - Microsoft, g9 - Google, i10 - ISRO, f11 - Facebook
-
-        // Getting Company Cost
-        company.getCompanyCost(2);
-
-        // Setting company's Share Price
-        company.setCompanySharePrice(300, 2);
-
-        // Getting company's Share Price
-        company.getCompanySharePrice(2);
-
-        // Getting Company's no. of shares
-        company.getCompanyNoOfShares(2);
-
-        // Setting Company's Service Rent
-        company.setCompanyServiceRent(500, 2);
-
-        // Getting Company's service rent
-        company.getCompanyServiceRent(2);
-
-        //Calling excess balance func
-        player.excessBalance(2);
-        
-        //Setting Player Current Position
-        player.setPlayerCurrentPosition(4,1);
-
-        //Getting Player Current Position
-        System.out.println(player.getPlayerCurrentPosition(1));
-
-
+        for(int i = 1; i>0 ; i++ ){
+            if(i%3==1){
+                i = 1;
+                System.out.println(i);
+            } 
+            else if(i%3==2){
+                i = 2;
+                System.out.println(i);
+            } 
+            else{
+                i = 3;
+                System.out.println(i);
+            }
+        }
+        */
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        System.out.flush();
+        System.out.println("Hello");
     }
 }
--- src/functions/Function.java
+++ src/functions/Function.java
@@ -8,9 +8,56 @@ public class Function {
     Player player = new Player();
 
     public int welcome() {
-        System.out.println("Welcome");
+        System.out.println("\n\n\n\t\t\t\t\tWelcome TO\n");
+        System.out.println("\t\t\t\t  Monopoly Stock Exchange\n\n");
+
         return 0;
     }
+
+     public int instructions(){
+        System.out.println(" Welcome to Monopoly Stock Exchange Game");
+        System.out.println(" Here are the rules of the game\n");
+        System.out.println("  1.This is a 3 player  modern stock trading game designed to be hassle-free and fun at every corner.\n ");
+        System.out.println("  2.Goal of this game is to become the first player to have more than Rs.20000 and win this ultimate stock market battle.The player will be declared bankrupt once he reaches Rs0.\n");
+        System.out.println("  3.This game has 12 blocks that start at 1 which is |GO|. Collect Rs.2000 every time the player passes 1(GO).  Players will have an option to move maximum of 3 blocks at a time.\n ");
+        System.out.println("  4.Land on the blocks numbered 2,5,6,8,10,11 to buy the company mentioned on the respective block by purchasing its shares.\n ");
+        System.out.println("  5.If a player lands on any of these |Company Block| which is owned by another player, the share value of that company will increase accordingly and the company owner will collect |service rent| from that player. \n");
+        System.out.println("  6.Blocks numbered 3,8 are the |Trading blocks|. Here players can trade and transfer their shares.\n ");
+        System.out.println("  7.If a player lands on block 4, they are caught in |Shortlisting Fraud|, pay fine of Rs.500 to the bank. \n");
+        System.out.println("  8.If a player lands on block 7, they face an |Income Tax Raid|, the share value of all the companies that player own will decrease by 5% along with a fine of Rs.200 to the bank. \n");
+        System.out.println("  9.Get lucky with a |Chance Card| every time the player lands on block 12. \n");
+        System.out.println(" 10.The players will have an option to sell their share anytime during the course of the game.\n");
+        return 0;
+     }
+
+     public int board(){
+         System.out.println("  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+         System.out.println(" |            |             |             |              |     ");
+         System.out.println(" |     1.     |      2.     |      3.     |      4.      |    ");
+         System.out.println(" |     GO     |   Amazon    |    Stock    |   Short      |        ");
+         System.out.println(" |            |             |    Market   |   Listing    |             ");
+         System.out.println(" |            |             |             |   Fraud      |                              ");
+         System.out.println(" |_ _ _ _ _ _ |_ _ _ _ _ _ _| _ _ _ _ _ _ |_ _ _ _ _ _ _ |   ");
+         System.out.println(" |            |                           |              | ");
+         System.out.println(" |    12.     |                           |      5.      |          ");
+         System.out.println(" |   Chance   |                           |    Tesla     |                        ");
+         System.out.println(" |            |                           |              |                ");
+         System.out.println(" |_ _ _ _ _ _ |                           | _ _ _ _ _ _ _|  ");
+         System.out.println(" |            |                           |              |  ");
+         System.out.println(" |     11.    |                           |      6.      |        ");
+         System.out.println(" |  Facebook  |                           |   Micorsoft  |                         ");
+         System.out.println(" |            |                           |              |               ");
+         System.out.println(" |_ _ _ _ _ _ | _ _ _ _ _ _ _ _ _ _ _ _ _ |_ _ _ _ _ _ _ |");
+         System.out.println(" |            |              |            |              |      ");
+         System.out.println(" |     10.    |      9.      |     8.     |      7.      |                ");
+         System.out.println(" |    ISRO    |    Google    |   Stock    |    Income    |              ");
+         System.out.println(" |            |              |   Market   |   Tax Raid   |                    ");
+         System.out.println(" |            |              |            |              |                         ");
+         System.out.println(" |_ _ _ _ _ _ |_ _ _ _ _ _ _ |_ _ _ _ _ _ |_ _ _ _ _ _ _ |                                             ");
+         System.out.println("                                          ");
+         return 0;
+     }
+     
 
     public String chance() {
         String[] chanceName = { "Hostile takeover attempt - pay lawyer 1000",
